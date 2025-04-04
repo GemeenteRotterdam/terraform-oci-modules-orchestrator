be8a82a9818083862bcf22a598fc14c7e4750f76
Andres Arias <aarias@quistor.com>
2025-04-04 09:24:34 +0000

    Add organization/owner support for the github repository. Required to be able to write outputs into github repo.

--- rms-facade/providers.tf
+++ rms-facade/providers.tf
@@ -25,4 +25,5 @@ provider "oci" {
 
 provider "github" {
   token = var.github_token != null ? var.github_token : (var.url_dependency_source_github_token != null ? var.url_dependency_source_github_token : null)
+  owner = var.github_configuration_owner
 }
