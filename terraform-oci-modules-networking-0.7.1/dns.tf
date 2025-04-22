c55a2be0bd126881c0186f0f7fff5ce5ebdfc157
Andres Arias <aarias@quistor.com>
2025-04-22 09:53:23 +0000

    Typo in dynamic rules

--- terraform-oci-modules-networking-0.7.1/dns.tf
+++ terraform-oci-modules-networking-0.7.1/dns.tf
@@ -292,10 +292,11 @@ resource "oci_dns_resolver" "these" {
       qname_cover_conditions    = rule.value.qname_cover_conditions != null ? rule.value.qname_cover_conditions : []
 
     }
+  }
   lifecycle {
     ignore_changes = [ resolver_id ]
   }
-  }
+
 
   depends_on = [data.oci_core_vcn_dns_resolver_association.dns_resolvers]
 }
